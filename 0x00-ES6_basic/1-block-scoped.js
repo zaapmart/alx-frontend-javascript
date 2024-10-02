43fda02204756f2e00a59361f6288d3740b5a496
Kufre Akpan <contactkufreakpan@gmail.com>
2024-10-02 07:23:04 -0700

    fixes

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
+/* eslint-disable */
+
 export default function taskBlock(trueOrFalse) {
-  const task = false; // use const for values that won’t change
-  const task2 = true; 
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;  // block-scoped, will not overwrite the outer 'task'
-    let task2 = false; // block-scoped, will not overwrite the outer 'task2'
+    task = true;
+    task2 = false;
   }
 
-  return [task, task2]; // returns the values from the outer scope
+  return [task, task2];
 }
