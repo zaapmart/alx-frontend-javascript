55de9845f75a390e06d8a475c2f116710d292173
Kufre Akpan <contactkufreakpan@gmail.com>
2024-10-04 10:16:32 -0700

    checkcer issue

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -10,7 +10,8 @@ export function getLast() {
 }
 
 export function taskNext() {
-  let combination = ' But sometimes let';
+  let combination = 'But sometimes let';
   combination += getLast();
+
   return combination;
 }
